ef4c1716f6445c719918ad71f0eed132fb0a0b0e
shangdelong <2924639689@qq.com>
2023-06-05 00:11:02 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/ataptiv/git/HelloGit.java
@@ -0,0 +1,10 @@
+package com.ataptiv.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("111!");
+        System.out.println("222!");
+
+    }
+}
